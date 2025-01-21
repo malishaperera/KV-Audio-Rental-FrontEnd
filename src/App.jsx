876bf0d9c1aa5673f517ce6fe47b4d2414840134
Malisha Perera <malishashavinda577@gmail.com>
2025-01-21 23:01:57 +0530

    Set up the frontend project and implemented ProductCard component with props for adding product details.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { ProductCard } from "./components/products"
-
+import ProductCard from "./components/products";
 
 function App() {
- 
-
   return (
     <>
-      <ProductCard/>
+      <ProductCard
+        photoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGEKk1DaaAsuqNnnM9yrZ_6OK6I7CiM1EFEg&s"
+        name="JBL Audio MINI 3"
+        price={40.8}
+        description="A compact and portable audio speaker with superior sound quality."
+      />
+
+      <ProductCard
+        photoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGEKk1DaaAsuqNnnM9yrZ_6OK6I7CiM1EFEg&s"
+        name="JBL Audio MINI 3"
+        price={40.8}
+        description="A compact and portable audio speaker with superior sound quality."
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,9 +1,12 @@
+import "./productCard.css";
 
-export function ProductCard(){
-    return(
-        <div>
-            <h1>Product Card</h1>
-        </div>
-    )
+export default function ProductCard(props) {
+  return (
+    <div className="product-card">
+      <img src={props.photoUrl} alt={props.name} />
+      <span className="product-name">{props.name}</span>
+      <span className="product-price">LKR. {props.price}</span>
+      <p className="product-description">{props.description}</p>
+    </div>
+  );
 }
-
